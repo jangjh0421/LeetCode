cb2401c8558c5e0c493131ad80d178cac3bf5b40
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-09-07 10:48:39 -0400

    first commit

---
+++ LC14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        shortest = strs[0]
+        commonPrefix = ""
+        exsits = True
+        all3 = True
+        for word in strs:
+            if len(word) < len(shortest):
+                shortest = word
+
+        for x in range(0, len(shortest)):
+            for string in strs:
+                if string[x] != shortest[x]:
+                    all3 = False
+            if all3 == True:
+                commonPrefix += shortest[x]
+
+        return commonPrefix
