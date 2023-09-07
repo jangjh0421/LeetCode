a427f930d8a51977ae9e30a98b897a87b76231a8
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-09-07 10:32:27 -0400

    second commit

--- LC9.py
+++ LC9.py
@@ -1,10 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        y = []
-        while x != 0:
-            y.append(x % 10)
-            x //= 10
-        for a in range(len(y)):
-            if y[a] != y[len(y) - 1 - a]:
+        s = str(x)
+        for a in range(len(s)):
+            if s[a] != s[len(s) - 1 - a]:
                 return False
+
         return True
