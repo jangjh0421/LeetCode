0ab20050d2edb2e829bcb4eb3a3d09830d28d727
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-09-09 14:08:00 -0400

    first commit

--- LC66.py
+++ LC66.py
@@ -1,23 +1,13 @@
-class Solution:
+class Solution(object):
     def plusOne(self, digits: list[int]) -> any:
-        dig = digits[0]
-        if digits[len(digits)-1] != 9:
-            digits[len(digits)-1] += 1
-
-        elif len(digits) == 1 and digits[0] == 9:
-            digits[0] = 0
-            digits.insert(0, 1)
-        else:
-            for x in range(len(digits)-1, -1, -1):
-                if x == 0:
-                    if digits[x] == 10 or digits[x] == 9:
-                        if dig == 9:
-                            digits[x] = 0
-                            digits.insert(0, 1)
-                    else:
-                        digits[x] += 1
-
-                elif digits[x] == 10 or digits[x] == 9:
-                    digits[x] = 0
-                    digits[x - 1] += 1
+        digits[-1] += 1
+        for x in range(1, len(digits) + 1):
+            if digits[-x] == 10:
+                digits[-x] = 0
+                try:
+                    digits[- (x + 1)] += 1
+                except IndexError:
+                    digits = [1] + digits
+            else:
+                break
         return digits
